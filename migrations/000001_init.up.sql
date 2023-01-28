052d9a13631274ef12edc75c935eafe627fb9f86
Konrad Amtenbrink <konrad02@gmx.de>
2023-01-28 15:23:00 +0100

    chore: update migrations

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -4,6 +4,5 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE documents (
     id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
     title TEXT NOT NULL,
-    url TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
